03e5d430d0f91462d09a4d5988bc49cd4178b3e5
MikeTatsuo <mike_tatsuo@hotmail.com>
2021-02-11 18:51:51 -0300

    intiial commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+#define SWITCH_PIN  2
+
+void setup() {
+  pinMode(SWITCH_PIN, INPUT);
+
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Mercury Tilt Switch");
+
+  lcd.setCursor(0, 2);
+  lcd.print("Status:");
+}
+
+void loop() {
+  lcd.setCursor(8, 2);
+
+  if (digitalRead(SWITCH_PIN)) {
+    lcd.print("OPEN  ");
+  } else {
+    lcd.print("CLOSED");
+  }
+
+  delay(150);
+}
